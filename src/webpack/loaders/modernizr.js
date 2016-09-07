d42f1f174456d9cf34d272a1d54be764cdf59f4d
Ray Patterson <ray_patterson@apple.com>
2016-09-07 12:36:54 -0700

    feat(debug): Disabled Customizr logs in dev mode.

--- src/webpack/loaders/modernizr.js
+++ src/webpack/loaders/modernizr.js
@@ -1,8 +1,14 @@
 'use strict';
 
+/**
+ * TODO: Inline Modernizr JS into `<head>`.
+ */
+
 const path = require('path');
 const fs = require('fs-jetpack');
 const combineLoaders = require('webpack-combine-loaders');
+
+const cfg = require('@raypatterson/sws-config');
 
 const projectRoot = require('../utils/get-project-root');
 const addHappyPackLoader = require('../utils/add-happy-pack-loader');
@@ -13,6 +19,15 @@ const resourceConfigPath = path.resolve(projectRoot, '.modernizr-autorc');
 module.exports = webpackConfig => {
 
 	if (fs.exists(resourceConfigPath)) {
+
+		if (cfg.debug === false) {
+
+			/**
+			 * NOTE: Disables output from Customizr
+			 */
+			process.argv.push('--quiet');
+
+		}
 
 		const LOADER_ID = 'modernizr';
 
