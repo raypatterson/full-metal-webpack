622241bbc9c6c6de94e1277cae55b7da12771d0b
Ray Patterson <ray_patterson@apple.com>
2016-09-08 10:43:01 -0700

    fix(linters): Possible fix for Stylelint.

--- cfg/stylelint.config.js
+++ cfg/stylelint.config.js
@@ -1,12 +1,8 @@
 'use strict';
 
-const path = require('path');
-
-const modulesPath = require('../src/webpack/utils/get-modules-path.js');
-
 module.exports = {
 	extends: [
-		path.join(modulesPath.node, 'stylelint-config-standard/index.js')
+		'stylelint-config-standard'
 	],
 	rules: {
 		indentation: 'tab'
